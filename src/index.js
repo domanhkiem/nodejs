7871831390be30de3252dbf06aa379d0dfff63fd
domanhkiem <domanhkiemnd@gmail.com>
2022-11-16 11:17:44 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const handlebars = require('express-handlebars').engine;
+const express = require('express');
+
+const methodOverride = require('method-override'); 
+
+
+const morgan = require('morgan');
+const { urlencoded } = require('express');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        helpers: {
+            sum:(a,b) => a+b,
+        }
+    
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+//Route inits
+route(app);
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
